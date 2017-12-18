a2f8f6ba672e44b293903b1a410ec1911468b04d
aBARICHELLO <arturbarichello@hotmail.com>
2017-12-18 11:17:16 -0200

    Server webpage

--- server/scoreboard.cpp
+++ server/scoreboard.cpp
@@ -1,15 +1,68 @@
 #include <cstring>
+#include <iostream>
 #include "SFML/Network.hpp"
+#include "JSON.h"
+
+void MakePacket(const wchar_t* action, const wchar_t* name, const float score, sf::Packet& packet) {
+    JSONObject data;
+    data[L"action"] = new JSONValue(action);
+    data[L"name"] = new JSONValue(name);
+    data[L"score"] = new JSONValue(packet);
+    
+    JSONValue* val = new JSONValue(data);
+    data.clear();
+    
+    std::wstring data_string = val->Stringify();
+    delete val;
+    
+    std::string wide;
+    wide.assign(data_string.begin(), data_string.end());
+    packet.append(wide.c_str(), wide.length());    
+}
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        return -1;
-    }
-
     sf::IpAddress ip("127.0.0.1");
     sf::UdpSocket socket;
     sf::Packet packet;
+    unsigned short port = 9000;
+    unsigned short response_port = 1002;
 
-    packet.append(argv[1], strlen(argv[1]));
-    socket.send(packet, ip, 9000);
+    // No arguments retrieve scores
+    if (argc == 1) {
+        MakePacket(L"GetScores",L"",0.0f,packet);
+        socket.bind(response_port);
+        socket.send(packet, ip, port);
+
+        char buffer[512];
+        sf::IpAddress response_ip;
+        std::size_t responde_size;
+        socket.receive(buffer, 512, responde_size, response_ip, port);
+        std::string response_string(buffer, responde_size);
+
+        JSONValue* json_highscore = JSON::Parse(response_string.c_str());
+        if (!json_highscore->IsObject()) {
+            std::cout << "Error" << "\n";
+            return -1;
+        }
+
+        JSONObject root = json_highscore->AsObject();
+        if (root.find(L"Scores") != root.end() && root[L"Scores"]->IsArray()) {
+            JSONArray scores = root[L"Scores"]->AsArray();
+            std::cout << "Current high scores: " << "\n";
+            for (int i = 0; i < scores.size(); ++i) {
+                JSONObject current_object = scores[i]->AsObject();
+                std::wcout << "Name: " << current_object[L"Name"]->AsString();
+                std::cout << "Highscore: " << current_object[L"Score"]->AsNumber() << "\n";
+            }
+        }
+        delete json_highscore;
+    } else if (argc == 3) {
+        MakePacket(L"AddScore", std::wstring(argv[1], argv[1] + strlen(argv[1])).c_str(), atof(argv[2]), packet);
+        if (socket.send(packet, ip, port) != sf::Socket::Done) {
+            std::cout << "An error occurred while sending the packet" << "\n";
+        }
+    } else {
+        std::cout << "Invalid usage" << "\n";
+        return -1;
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,94 @@
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+var dgram = require('dgram'),
+file_system = require('fs'),
+highscores,
+server;
 
-console.log("Socket created\n");
-server.on("message", function(msg, rinfo) {
-   console.log("Received message" + msg.toString()); 
+file_system.readFile("highscores.txt", 'utf8', function(err, data) {
+    if (err) {
+        console.log("Error ocurred while handling file");
+        process.exit();
+    }
+
+    console.log("Loading highscores...");
+
+    try {
+        highscores = JSON.parse(data);
+    } catch (e) {
+        console.log("Exception ocurred on JSON");
+        process.exit();
+    }
+
+    highscores.Scores.sort(function(a, b) {
+        return b.Score - a.Score;
+    });
+
+    console.log(highscores);
 });
+
+server = dgram.createSocket('udp4');
+console.log("Socket created\n");
+
+server.on("message", function(msg, rinfo) {
+   console.log(rinfo);
+
+    if (rinfo.size != 4) {
+        console.log("Received message" + msg.toString());
+        var jsonData, i;
+
+        try {
+            jsonData = JSON.parse(msg);
+        } catch (exception) {
+            console.log("Invalid JSON request");
+            return;
+        }
+
+        switch (jsonData.action) {
+            case "AddScore":
+                console.log("AddScore called\n");
+            
+                if (highscores != undefined) {
+                    for (i = 0; i < highscores.Scores.length; ++i) {
+                        if (highscores.Scores[i].Score < jsonData.score) {
+                            highscores.Scores.splice(i, 0, {"Name" : jsonData.name, "Score" : jsonData.score});
+                            console.log("Inserted highscore by: " + jsonData.name);
+                            break;
+                        }
+                    }
+                }
+                console.log(highscores.Scores);
+                break;
+
+            case "GetScores":
+                console.log("Getting scores called...");
+                var highscore_string = JSON.stringify(highscores);
+                var response_buffer = new Buffer(highscore_string.length);
+                response_buffer.write(highscore_string);
+
+                server.send(response_buffer, 0, response_buffer.length, rinfo.port, rinfo.address, function(err, sent) {
+                    if (err)
+                        console.log("Error sending response");
+                    else
+                        console.log("Responded to client at " + r.info.address + ":" + rinfo.port);
+                });
+                break;
+        }
+    }
+});
+
+server.on("listening", function() {
+    var address = server.address();
+    console.log("Server listening " + address.address + ":" + address.port);
+});
+
 server.bind(9000);
+
+var web_server = require('http');
+web_server.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("<html><body><h1>High Scores</h1><ul>");
+    for (i = 0; i < highscores.Scores.length; ++i) {
+        response.write(highscores.Scores[i].Name + "&nbsp;&nbsp;" + highscores.Scores[i].Score + "<br />");
+    }
+    response.write("</ul></body></html>");
+    response.end();
+}).listen(80);
